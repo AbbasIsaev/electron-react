20893619ddb16c7ff86dff46a917414156080677
isaev <abbas.isaev@gmail.com>
2023-08-09 13:07:00 +0300

    add build client

---
+++ src/config.ts
@@ -0,0 +1,2 @@
+export const IS_DEV = true
+console.log('IS_DEV->', IS_DEV)
--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,10 +1,27 @@
 import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import * as path from "path";
+import {IS_DEV} from "./src/config";
+import CopyPlugin = require("copy-webpack-plugin");
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
-export const plugins = [
+export const plugins: any = [
     new ForkTsCheckerWebpackPlugin({
         logger: 'webpack-infrastructure',
     }),
 ];
+
+if (!IS_DEV) {
+    // Копирование статических фалов
+    const copyPlugin = new CopyPlugin({
+        patterns: [
+            {
+                from: path.resolve(__dirname, 'client', 'build'),
+                to: path.resolve(__dirname, '.webpack/renderer')
+            }
+        ]
+    });
+
+    plugins.push(copyPlugin);
+}
